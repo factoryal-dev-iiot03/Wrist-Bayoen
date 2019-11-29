1efffda8b80097e24c141760f88e19a43652b776
yewl1110 <yewl1110@naver.com>
2019-11-29 19:09:13 +0900

    Update TCPClient.java

--- PC/TCPExample/TCPClient/src/TCPClient.java
+++ PC/TCPExample/TCPClient/src/TCPClient.java
@@ -18,34 +18,4 @@ public static void main(String argv[]) throws Exception {
   } while(!sentence.equals("-1\n"));
   clientSocket.close();
  }
-/*
- public static void main(String[] args) {
-        String hostname = "time.nist.gov";
-        int port = 13;
- 
-        try (Socket socket = new Socket(hostname, port)) {
- 
-            InputStream input = socket.getInputStream();
-            InputStreamReader reader = new InputStreamReader(input);
- 
-            int character;
-            StringBuilder data = new StringBuilder();
- 
-            while ((character = reader.read()) != -1) {
-                data.append((char) character);
-            }
- 
-            System.out.println(data);
- 
- 
-        } catch (UnknownHostException ex) {
- 
-            System.out.println("Server not found: " + ex.getMessage());
- 
-        } catch (IOException ex) {
- 
-            System.out.println("I/O error: " + ex.getMessage());
-        }
-    }
-*/
 }
