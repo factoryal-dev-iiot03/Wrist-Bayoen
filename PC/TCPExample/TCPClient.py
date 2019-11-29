6593faafefc2eb017a1c60efbfdbb971eb033182
yewl1110 <yewl1110@naver.com>
2019-11-29 21:32:33 +0900

    Create TCPClient.py

---
+++ PC/TCPExample/TCPClient.py
@@ -0,0 +1,16 @@
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 6789        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input()
+        if message=='-1': break
+        message += '\n'
+        s.sendall(bytes(message, 'utf-8'))
+        data = s.recv(1024) #.decode('utf-8')
+        data.decode('utf-8')
+        print('Received', repr(data))
