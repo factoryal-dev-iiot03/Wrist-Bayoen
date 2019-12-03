dabdc45124e69273218698076adcd82a969c0248
yewl1110 <yewl1110@naver.com>
2019-12-03 21:01:20 +0900

    Add Threadpool

---
+++ PC/Bayoen-Server/src/Client.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client extends Thread{
+	String clientSentence;
+	String capitalizedSentence;
+	Socket connectionSocket;
+	
+	KeyboardControl keyboardControl;
+	
+	public Client(Socket socket) {
+		//System.out.println(socket);
+		this.connectionSocket = socket;
+		keyboardControl = new KeyboardControl();
+	}
+	
+	public void run() {
+		try {
+			BufferedReader inFromClient =
+			new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+					   
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			
+			do {
+				clientSentence = inFromClient.readLine();
+				
+				for (int i = 0; i < clientSentence.length(); i++) {
+					keyboardControl.control(clientSentence.charAt(i));
+				}
+				
+				capitalizedSentence = clientSentence.toUpperCase() + '\n';
+				outToClient.writeBytes(capitalizedSentence);
+				//System.out.println(clientSentence.equals("-1"));
+			}while(!clientSentence.equals("-1"));
+			
+			connectionSocket.close();
+		}catch(Exception e) {
+			
+		}finally { 
+			try {
+				this.finalize();
+			} catch (Throwable e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	public void stopThread() {
+		try {
+			this.finalize();
+		} catch (Throwable e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
---
+++ PC/Bayoen-Server/src/ClientManager.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ClientManager implements Runnable{
+	ArrayList<Client> clients;
+	int devices;
+	
+	public ClientManager() {
+		clients = new ArrayList<>();
+		devices = 0;
+	}
+	
+	public void addClient(Client client) {
+		if(devices <= 2) {
+			client.start();
+			clients.add(client);
+			devices++;
+		} else {
+			client.stopThread();
+		}
+	}
+	
+	public void run() {
+		while(true) {
+			for (Client client:clients) {
+				/*if(client.getState() == Thread.State.TERMINATED) {
+					client.stopThread();
+					clients.remove(client);
+					devices--;
+				}*/
+				System.out.println(client.getState());
+			}
+			
+			try {
+				System.out.println(devices);
+				
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
